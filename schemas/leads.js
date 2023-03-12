4c98ad183c185ed51831b057ca891251104b40c9
Lokendra Soni <lsoni@dashclicks.com>
2023-03-12 20:57:14 +0530

    Added more sorting options

--- schemas/leads.js
+++ schemas/leads.js
@@ -5,7 +5,7 @@ exports.leads = {
         type: Joi.string().valid("", "all", "hot", "cold", "warm").optional(),
         page: Joi.number().default(1),
         limit: Joi.number().default(15),
-        sortField: Joi.string().valid("", "name", "company", "sent", "opened", "responded").optional(),
+        sortField: Joi.string().valid("", "name", "company", "sent", "opened", "responded", "created_at", "updated_at").optional(),
         sortOrder: Joi.string().valid("", "asc", "desc").optional()
     })
 };
